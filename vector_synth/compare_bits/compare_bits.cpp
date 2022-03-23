8c7bc60dc5f70b04bfbed2aa1193a74d448a827e
Kabir Sheth <kabirsheth@MacBook-Pro.local>
2022-03-23 15:18:21 -0400

    second iteration of compare_bits

--- vector_synth/compare_bits/compare_bits.cpp
+++ vector_synth/compare_bits/compare_bits.cpp
@@ -13,24 +13,28 @@ ctxt less_than_bits(std::vector<ctxt> ctext1, std::vector<ctxt> ctext2, const ui
     ctxt negated1;
     ctxt negated2;
     ctxt temp;
+    ctxt temp2;
     
     seal::RelinKeys rk = info.keys->rk;
 
     for(int i = l - 1; i >= 0; i--){
         info.eval->negate(ctext1[i], temp);
-        LT = temp * ctext2[i];
+        info.eval->multiply(temp, ctext2[i], LT);
         std::vector<ctxt> mult_parts(i);
         for(int j = i - 1; j >= 0; j--){
             info.eval->negate(ctext1[j], negated1);
             info.eval->negate(ctext2[j], negated2);
-            mult_parts[j] = ((ctext1[j] * ctext2[j]) + (negated1 * negated2));
+            info.eval->multiply(ctext1[j], ctext2[j], temp);
+            info.eval->multiply(negated1, negated2, temp2);
+            info.eval->add(temp, temp2, mult_parts[j]);
         }
         info.eval->multiply_many(mult_parts, rk, LocalPRODUCT);
-        sum_parts[i] = LT * LocalPRODUCT;
+        info.eval->multiply(LT, LocalPRODUCT, sum_parts[i]);
     }
     info.eval->add_many(sum_parts, SUM);
+    info.eval->exponentiate(SUM, ((*p)-1), rk, temp);
 
-    return (SUM ^ (*p-1)) % *p;
+    return temp;
 }
 
 int main(){
