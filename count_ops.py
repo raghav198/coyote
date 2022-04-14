8cd2695d372931dfad1ec2f9289ae1ad63a625ec
malik22 <malik22@pal-nat186-173-145.itap.purdue.edu>
2022-04-13 22:00:48 -0400

    counting subtracts

--- count_ops.py
+++ count_ops.py
@@ -1,19 +1,21 @@
 import os
 
 count = lambda op, code: sum(map(lambda l: op in l, code))
-lines = ['Benchmark,Scalar Add,Scalar Mul,Vector Add,Vector Mul,Rotate,Blend\n']
+lines = ['Benchmark,Scalar Add,Scalar Subtract,Scalar Mul,Vector Add,Vector Subtract,Vector Mul,Rotate,Blend\n']
 for benchmark in os.listdir('outputs/'):
     scal = open(f'outputs/{benchmark}/scal').read().splitlines()
     vec = open(f'outputs/{benchmark}/vec').read().splitlines()
 
     smul = count('*', scal)
+    ssub = count('-', scal)
     sadd = count('+', scal)
 
     vmul = count('*', vec)
     vadd = count('+', vec)
+    vsub = count('-', vec)
     vrot = count('>>', vec)
     vblend = count('blend', vec)
-    lines.append(f'{benchmark},{sadd},{smul},{vadd},{vmul},{vrot},{vblend}\n')
+    lines.append(f'{benchmark},{sadd},{ssub},{smul},{vadd},{vsub},{vmul},{vrot},{vblend}\n')
 
     print(f'{benchmark}:\t({smul} *, {sadd} +)\t->\t({vmul} *, {vadd} +, {vrot} >, {vblend} @)')
 
