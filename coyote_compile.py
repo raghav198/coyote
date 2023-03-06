67ad2eac2eb2fd6a6cdb375275a7b87811741eae
malik22 <malik22@pal-nat186-173-145.itap.purdue.edu>
2023-03-06 11:42:27 -0500

    fixed coyote compile

--- coyote_compile.py
+++ coyote_compile.py
@@ -1,8 +1,9 @@
 import argparse
-
+from time import time
 import os
 from coyote import coyote_compiler
 import importlib, pathlib
+import fnmatch
 from compile_to_bfv import compile_scalar, compile_vector
 
 
@@ -41,12 +42,18 @@ def main():
     cpp_group = parser.add_mutually_exclusive_group()
     cpp_group.add_argument('--no-cpp', action='store_true', help='Stop before generating C++')
     cpp_group.add_argument('--just-cpp', action='store_true', help='Use precompiled Coyote code to directly generate C++')
+    
+    parser.add_argument('-u', '--update-cmake', action='store_true', help='Update CMakeLists.txt with circuits')
 
     # TODO: expose some compiler options in the API (i.e. verbosity, turning features on/off, number of rounds, etc.) and then expose those here
 
     args = parser.parse_args()
 
-    coyote = get_coyote(args.coyote_file)
+    try:
+        coyote = get_coyote(args.coyote_file)
+    except ImportError as e:
+        parser.error(f'{e}')
+        return
 
     available = [func.__name__ for func in coyote.func_signatures]
 
@@ -58,22 +65,28 @@ def main():
 
     output_dir = args.output_dir
 
-    for circuit in args.circuits:
-        if circuit not in available:
-            parser.error(f'Coyote file `{args.coyote_file}` does not define `{circuit}` as a circuit!')
+    # for circuit in args.circuits:
+    #     if circuit not in available:
+    #         parser.error(f'Coyote file `{args.coyote_file}` does not define `{circuit}` as a circuit!')
      
 
     if args.combine:
         print('...no <3')
         return
     
-    scalars = {}
-    vectors = {}
+    scalars: dict[str, list[str]] = {}
+    vectors: dict[str, list[str]] = {}
 
     if not args.just_cpp and not os.path.exists(args.backend_dir):
         parser.error(f'Backend not found at {args.backend_dir}!')
 
+    # preprocess circuit list
+    
+    circuits = []
     for circuit in args.circuits:
+        circuits += fnmatch.filter(available, circuit)
+
+    for circuit in circuits:
 
         if args.just_cpp:
             if not (os.path.exists(os.path.join(output_dir, circuit)) and os.path.exists(os.path.join(output_dir, circuit, 'scal')) and os.path.exists(os.path.join(output_dir, circuit, 'vec'))):
@@ -86,27 +99,30 @@ def main():
                 print(f'Warning: overwriting {circuit}')
             else:
                 os.mkdir(os.path.join(output_dir, circuit))
+            print(f'Building {args.coyote_file}/{circuit}...')
+            start = time()
             scalar = coyote.instantiate(circuit)
             vector = coyote.vectorize()
+            print(f'Finished in {time() - start:.2f}s')
 
             scalars[circuit] = scalar
-            vectors[circuit] = vector
+            vectors[circuit] = list(map(str, vector))
 
             open(os.path.join(output_dir, circuit, 'scal'), 'w').write('\n'.join(scalar))
-            open(os.path.join(output_dir, circuit, 'vec'), 'w').write('\n'.join(vector))
+            open(os.path.join(output_dir, circuit, 'vec'), 'w').write('\n'.join(map(str, vector)))
 
     if not args.no_cpp:
-        for circuit in args.circuits:
-            if os.path.exists(os.path.join(args.backend_dir, circuit)):
+        for circuit in circuits:
+            if os.path.exists(os.path.join(args.backend_dir, 'coyote_out', circuit)):
                 print(f'Warning: overwriting C++ for {circuit}')
             else:
-                os.mkdir(os.path.join(args.backend_dir, circuit))
+                os.makedirs(os.path.join(args.backend_dir, 'coyote_out', circuit))
 
             scalar_cpp = compile_scalar(scalars[circuit])
             vector_cpp = compile_vector(vectors[circuit])
 
-            open(os.path.join(args.backend_dir, circuit, 'scalar.cpp'), 'w').write(scalar_cpp)
-            open(os.path.join(args.backend_dir, circuit, 'vector.cpp'), 'w').write(vector_cpp)
+            open(os.path.join(args.backend_dir, 'coyote_out', circuit, 'scalar.cpp'), 'w').write(scalar_cpp)
+            open(os.path.join(args.backend_dir, 'coyote_out', circuit, 'vector.cpp'), 'w').write(vector_cpp)
 
 
 if __name__ == '__main__':
