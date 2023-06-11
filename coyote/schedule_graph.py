f05adecf1c97c6b13ef7212d59029c5226facfa1
malik22 <malik22@pal-nat186-173-145.itap.purdue.edu>
2023-06-11 14:37:25 -0400

    refactor

---
+++ coyote/schedule_graph.py
@@ -0,0 +1,157 @@
+from collections import defaultdict
+from typing import cast
+import networkx as nx
+
+from .coyote_ast import Instr
+from .disjoint_set import DisjointSet
+
+def instr_sequence_to_nx_graph(instrs: list[Instr]) -> nx.DiGraph:
+    graph = nx.DiGraph()
+    graph.graph['ops'] = {}
+    for instr in instrs:
+        graph.graph['ops'][instr.dest.val] = instr.op
+        # if instr.op == '~':
+        #     continue
+        if instr.op == '~' and not (instr.lhs.reg and instr.rhs.reg):
+            continue
+        graph.add_edge((instr.lhs.val,), (instr.dest.val,))
+        graph.add_edge((instr.rhs.val,), (instr.dest.val,))
+        
+    return graph
+
+
+def grade_nx_graph(graph: nx.DiGraph, groups: list[set[int]]):
+    for node in graph:
+        if 'epoch' in graph.nodes[node]:
+            del graph.nodes[node]['epoch']
+
+    for i, group in enumerate(groups):
+        # print(f'Setting {group} to {i}')
+        for node in group:
+            graph.nodes[(node,)]['epoch'] = i
+
+    def visit(node: int):
+        if 'epoch' in graph.nodes[node]:
+            return
+        children = {c for c, _ in graph.in_edges(node)} # type: ignore
+        heights = set()
+        for child in children:
+            if 'epoch' not in graph.nodes[child]:
+                visit(child)
+            heights.add(graph.nodes[child]['epoch'] + 1)
+        graph.nodes[node]['epoch'] = max(heights | {len(groups)})
+
+    for node in graph:
+        visit(node)
+
+
+def producers(graph: nx.DiGraph, nbunch) -> set[int]:
+    return {p for p, _ in graph.in_edges(nbunch)} # type: ignore
+
+
+def nx_columnize(_graph: nx.DiGraph, force_lanes: dict[int, int]):
+    # graph = nx.quotient_graph(_graph.to_undirected(), lambda u, v: u in force_lanes and v in force_lanes and force_lanes[u] == force_lanes[v])
+    # graph = _graph
+    graph: nx.Graph = cast(nx.Graph, _graph.to_undirected())
+
+    epochs: dict[int, list[int]] = defaultdict(list)
+    for node in graph:
+        epochs[_graph.nodes[node]['epoch']].append(node)
+
+    num_epochs = max(epochs.keys()) + 1
+
+    # print('epochs:', epochs)
+    
+
+    # bipartite pieces, indexed by (source, target) epoch
+    pieces: dict[tuple[int, int], nx.graph.Graph] = {}
+    for i in range(num_epochs): # i = source epoch
+        for j in range(i + 1, num_epochs): # j = target epoch
+            # part1 = set(epochs[i])
+            # part2 = consumers(_graph, epochs[i]).intersection(set(epochs[j]))
+
+            part1 = set(epochs[j])
+            part2 = producers(_graph, epochs[j]).intersection(epochs[i])
+
+            # part2 = epochs[j]
+
+            # print(f'Parts of {i, j}: {part1, part2}')
+
+            # have to make a copy so that it can be modified independently
+            bp_subgraph: nx.Graph = nx.Graph(graph.subgraph(part1 | part2))
+            bp_subgraph.add_nodes_from(part1, bipartite=0)
+            bp_subgraph.add_nodes_from(part2, bipartite=1)
+
+            if bp_subgraph.number_of_edges() == 0:
+                # nothing to see here, moving on...
+                continue
+
+            for x in part1:
+                for y in part2:
+                    weight = bp_subgraph.degree[x] + bp_subgraph.degree[y]
+                    if bp_subgraph.has_edge(x, y):
+                        # print(f'Weighting {x, y}')
+                        bp_subgraph[x][y]['weight'] = weight
+
+            # print(f'Putting {bp_subgraph.edges} in for {i, j}')
+            pieces[i, j] = bp_subgraph
+            # print(f'{i, j} weights: {bp_subgraph.edges(data=True)}')
+    # print(pieces)
+    columns: DisjointSet[tuple[int]] = DisjointSet()
+    total_degree = 0
+    
+    preloaded: dict[int, set[tuple[int]]] = defaultdict(set)
+    for node in force_lanes:
+        preloaded[force_lanes[node]].add((node,))
+    for group in preloaded.values():
+        # print(f'Grouping {group} into a column')
+        columns.new_class(*group)
+        
+    # print('in grading, the following are already put in columns:')
+    # for cls in columns.all_classes():
+    #     print(cls)
+
+    for i, j in sorted(pieces.keys()):
+        bp_piece = pieces[i, j]
+        # print(f'Full bp piece {i, j}: {bp_piece.edges}')
+        part = set(n for n, d in bp_piece.nodes(data=True) if d['bipartite']) # type: ignore
+
+        # TODO: this is not the right condition for marking an edge 'unmatchable'
+        ## also check if the edge connects an unmatched vertex to one already matched with something in the same epoch
+        matchable_graph = nx.graphviews.subgraph_view(bp_piece, filter_edge=lambda u, v: not (columns.contains(u) and columns.contains(v))) # type: ignore
+        # matchable_graph = nx.graphviews.subgraph_view(matchable_graph, filter_edge=lambda u, v: not (u in force_lanes and v in force_lanes and force_lanes[u] != force_lanes[v]))
+        
+        # print(f'Marking edges {[(u, v) for u, v in bp_piece.edges if columns.contains(u) and columns.contains(v)]} as unmatchable')
+        # print(f'{matchable_graph.edges} are all matchable')
+        matching = nx.algorithms.max_weight_matching(matchable_graph, maxcardinality=True)
+
+
+        # print(f'Querying weights for {matching}')
+        weight = sum(bp_piece[u][v]['weight'] for u, v in matching)
+        # print(f'Matching for {i, j}: {matching} (weight={weight})')
+        
+
+        for u, v in matching:
+            assert not (columns.contains(u) and columns.contains(v)), (u, v)
+
+            if columns.contains(u):
+                columns.add_to(v, u)
+            elif columns.contains(v):
+                columns.add_to(u, v)
+            else:
+                columns.add(u)
+                columns.add_to(v, u)
+
+        columns.add(*filter(lambda p: not columns.contains(p), part))
+
+        rotation_graph = nx.graphviews.subgraph_view(graph, filter_edge=lambda u, v: (u, v) not in matching and (v, u) not in matching) # type: ignore
+
+        total_degree += max(rotation_graph.degree(), key=lambda n: n[1])[1]
+    # list(map(print, map(sorted, columns.all_classes())))
+    # quit()
+    
+    for i, col in enumerate(columns.all_classes()):
+        for node in col:
+            _graph.nodes[node]['column'] = i
+
+    return columns
---
+++ coyote/search/protoschedule.py
@@ -0,0 +1,173 @@
+from collections import Counter, defaultdict
+from dataclasses import dataclass, field
+from functools import reduce
+from heapq import heappop, heappush
+from typing import Optional, cast
+
+from ..schedule_graph import grade_nx_graph, nx_columnize
+from .lane_placement import lane_placement
+
+import networkx as nx
+
+
+MUL_PER_ROTATE = 1
+ADD_PER_ROTATE = 0.1
+
+COSTS_PER_ROTATE = defaultdict(int, {'+': ADD_PER_ROTATE, '*': MUL_PER_ROTATE, '-': ADD_PER_ROTATE})
+
+
+def contract_edge(graph: nx.DiGraph, edge):
+    u, v = edge
+    contracted = nx.contracted_edge(graph, edge, self_loops=False)
+    return nx.relabel_nodes(contracted, {u: u + v}, copy=False)
+
+def schedule_height(graph: nx.DiGraph, debug=False):
+    cells: dict[int, dict[int, tuple]] = defaultdict(lambda: defaultdict(tuple))
+
+    for node in graph:
+        cells[graph.nodes[node]['epoch']][graph.nodes[node]['column']] += node
+
+    columns: dict[int, tuple] = defaultdict(tuple)
+    for node in graph:
+        columns[graph.nodes[node]['column']] += node
+
+
+    instr_counts: Counter = sum((reduce(lambda x, y: x | y, (Counter(graph.graph['ops'][instr] for instr in column) for column in epoch.values())) for epoch in cells.values()), Counter())
+    cost = sum(COSTS_PER_ROTATE[op] * count for op, count in instr_counts.items())
+    if debug:
+        print(f'Instruction counts: {instr_counts}; {cost}')
+
+    return cost
+
+
+
+def pq_relax_schedule(graph: nx.DiGraph, groups: list[set[int]], force_lanes: dict[int, int], rounds=200):
+    @dataclass(order=True)
+    class schedule:
+        cost: int | float
+        graph: nx.DiGraph=field(compare=False)
+        edges: Optional[list[tuple]]=field(compare=False)
+
+    grade_nx_graph(graph, groups)
+    nx_columnize(graph, force_lanes)
+    # print(force_lanes)
+    # print('AFTER COLUMNIZING')
+    # print(graph.nodes(data=True))
+    # print(f'Original rotation cost: {rotation_cost(graph)}')
+    new_data = {}
+    for reg, lane in force_lanes.items():
+        # print(f'Setting {graph.nodes[reg,]["column"]} to {lane}')
+        col_num = graph.nodes[reg,]['column']
+        new_data.update({n: lane for n, d in graph.nodes(data=True) if d['column'] == col_num})
+        # graph.nodes[reg,]['column'] = lane
+    nx.set_node_attributes(graph, new_data, name='column')
+
+    # print('FIRST FIX')
+    # print(graph.nodes(data=True))
+
+    cost_history = []
+    best_history = []
+
+    # print(rotation_cost(graph))
+    # input()
+    current_cost = lane_placement(graph, force_lanes, t=50, beta=0.001, rounds=50000)
+    current_cost += schedule_height(graph)
+    
+    # print('ONE ROUND OF SA')
+    # print(graph.nodes(data=True))
+    # print(rotation_cost(graph))
+    # input()
+    
+    for reg, lane in force_lanes.items():
+        # print(f'Setting {graph.nodes[reg,]["column"]} to {lane}')
+        # col_num = graph.nodes[reg,]['column']
+        # new_data = {n: lane for n, d in graph.nodes(data=True) if d['column'] == col_num}
+        graph.nodes[reg,]['column'] = lane
+        # nx.set_node_attributes(graph, new_data, name='column')
+
+    # print('SECOND FIX')
+    # print(graph.nodes(data=True))
+    best = schedule(cost=current_cost, graph=nx.DiGraph(graph), edges=None)
+    
+    pqueue: list[schedule] = []
+    heappush(pqueue, schedule(cost=best.cost, graph=best.graph, edges=None))
+    for r in range(rounds):
+        if not len(pqueue):
+            # print('No more graphs to try!')
+            break
+
+        cur = heappop(pqueue)
+
+        best_history.append(best.cost)
+        cost_history.append(cur.cost)
+        # if len(pqueue):
+        #     print(f'Round {r}/200: exploring {cur.cost}{" (new best!)" if cur < best else ""}')
+        if cur < best:
+            best = schedule(cost=cur.cost, graph=nx.DiGraph(cur.graph), edges=None)
+
+        if cur.edges is None:
+            # generate all candidate solutions
+            cross_edges = set()
+            for u, v in cur.graph.edges:
+                src = cur.graph.nodes[u]['epoch']
+                span = cur.graph.nodes[v]['column'] - cur.graph.nodes[u]['column']
+
+                if span == 0: continue
+                if src < len(groups): continue
+                cross_edges.add((u, v))
+
+            cur.edges = list(cross_edges)
+        
+        if not len(cur.edges):
+            continue
+            
+        edge_to_contract = None
+        # grab an edge, remove it from the list, and add its contraction to the queue
+        while True:
+            if not cur.edges:
+                break
+            edge_to_contract = cur.edges.pop()
+            
+            left_fixed = set(edge_to_contract[0]).intersection(set(force_lanes.keys()))
+            right_fixed = set(edge_to_contract[1]).intersection(set(force_lanes.keys()))
+            if left_fixed and right_fixed and force_lanes[next(iter(left_fixed))] != force_lanes[next(iter(right_fixed))]:
+                continue
+            break
+        if edge_to_contract is None: # none of the candidates were free
+            continue
+        
+        # for edge_to_contract in cross_edges:
+            # print(f'\tedge {edge_to_contract}...')
+        raw_contracted = contract_edge(cur.graph, edge_to_contract)
+        contracted = cast(nx.DiGraph, nx.condensation(raw_contracted))
+
+        for node in contracted:
+            fixed = set(sum(contracted.nodes[node]['members'], ())).intersection(set(force_lanes.keys()))
+            if fixed:
+                contracted.nodes[node]['column'] = force_lanes[next(iter(fixed))]
+            else:
+                contracted.nodes[node]['column'] = raw_contracted.nodes[next(iter(contracted.nodes[node]['members']))]['column']
+
+        contracted.graph['ops'] = cur.graph.graph['ops']
+    
+        relabeling = {num : sum(contracted.nodes[num]['members'], ()) for num in contracted}
+        contracted = nx.relabel_nodes(contracted, relabeling)
+
+        grade_nx_graph(contracted, groups)
+        contracted_cost = lane_placement(contracted, force_lanes, t=50, beta=0.001, rounds=20000)
+        contracted_cost += schedule_height(contracted)
+
+        heappush(pqueue, schedule(cost=contracted_cost, graph=nx.DiGraph(contracted), edges=None))
+        if len(cur.edges): # if there are still unexplored edges
+            heappush(pqueue, schedule(cost=cur.cost, graph=nx.DiGraph(cur.graph), edges=cur.edges[:])) # put this back into the queue
+
+    """from matplotlib import pyplot as plt
+    plt.plot(cost_history)
+    plt.plot(best_history)
+    plt.show()"""
+
+    with open('trace.csv', 'w') as f:
+        f.write(','.join(map(str, cost_history)) + '\n')
+        f.write(','.join(map(str, best_history)) + '\n')
+
+    return best
